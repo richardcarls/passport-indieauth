bdc98419502bbc9cc81a03657b21c90f36ac80ec
Richard Carls <richard.j.carls@gmail.com>
2016-05-20 17:30:17 -0400

    Fix indentation, no more mixed tabs and spaces

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -9,82 +9,82 @@ var mockRedirectUri = 'https://example-client.com/auth';
 
 describe('@rcarls/passport-indieauth', function() {
 
-  describe('constructed', function() {
+    describe('constructed', function() {
 
-    describe('with minimum options and verify callback', function() {
+	describe('with minimum options and verify callback', function() {
 
-      var strategy = new IndieAuthStrategy({
-	clientId: mockClientId + '/',
-	redirectUri: mockRedirectUri,
-      }, function() {});
-      
-      it('should be named "indieauth"', function() {
-	expect(strategy.name).to.equal('indieauth');
-      });
-      
-    }); // with minimum options
+	    var strategy = new IndieAuthStrategy({
+		clientId: mockClientId + '/',
+		redirectUri: mockRedirectUri,
+	    }, function() {});
+	    
+	    it('should be named "indieauth"', function() {
+		expect(strategy.name).to.equal('indieauth');
+	    });
+	    
+	}); // with minimum options
 
-    describe('without options', function() {
+	describe('without options', function() {
 
-      it('should throw', function() {
-	expect(function() {
-	  var strategy = new IndieAuthStrategy(function(){});
-	}).to.throw(TypeError, 'IndieAuthStrategy requires an options object');
-      });
-      
-    }); // without options
+	    it('should throw', function() {
+		expect(function() {
+		    var strategy = new IndieAuthStrategy(function(){});
+		}).to.throw(TypeError, 'IndieAuthStrategy requires an options object');
+	    });
+	    
+	}); // without options
+	
+	describe('without a verify callback', function() {
+	    
+	    it('should throw', function() {
+		expect(function() {
+		    var strategy = new IndieAuthStrategy({
+			clientId: mockClientId + '/',
+			redirectUri: mockRedirectUri,
+		    });
+		}).to.throw(TypeError, 'IndieAuthStrategy requires a verify callback');
+	    });
+	    
+	}); // without a verify callback
+
+	describe('without a clientId option', function() {
+	    
+	    it('should throw', function() {
+		expect(function() {
+		    var strategy = new IndieAuthStrategy({
+			redirectUri: mockRedirectUri,
+		    }, function() {});
+		}).to.throw(TypeError, 'IndieAuthStrategy requires a clientId option');
+	    });
+	    
+	}); // without a clientId option
+
+	describe('without a redirectUri option', function() {
+	    
+	    it('should throw', function() {
+		expect(function() {
+		    var strategy = new IndieAuthStrategy({
+			clientId: mockClientId + '/',
+		    }, function() {});
+		}).to.throw(TypeError, 'IndieAuthStrategy requires a redirectUri option');
+	    });
+	    
+	}); // without a redirectUri option
+
+	describe('with an invalid responseType', function() {
+
+	    it('should throw', function() {
+		expect(function() {
+		    var strategy = new IndieAuthStrategy({
+			clientId: mockClientId + '/',
+			redirectUri: mockRedirectUri,
+			responseType: 'invalid',
+		    }, function() {});
+		}).to.throw(TypeError, 'response_type must be one of either "id" or "code"');
+	    });
+	    
+	}); // with an invalid responseType
+	
+    });
     
-    describe('without a verify callback', function() {
-      
-      it('should throw', function() {
-	expect(function() {
-	  var strategy = new IndieAuthStrategy({
-	    clientId: mockClientId + '/',
-	    redirectUri: mockRedirectUri,
-	  });
-	}).to.throw(TypeError, 'IndieAuthStrategy requires a verify callback');
-      });
-      
-    }); // without a verify callback
-
-    describe('without a clientId option', function() {
-      
-      it('should throw', function() {
-	expect(function() {
-	  var strategy = new IndieAuthStrategy({
-	    redirectUri: mockRedirectUri,
-	  }, function() {});
-	}).to.throw(TypeError, 'IndieAuthStrategy requires a clientId option');
-      });
-      
-    }); // without a clientId option
-
-    describe('without a redirectUri option', function() {
-      
-      it('should throw', function() {
-	expect(function() {
-	  var strategy = new IndieAuthStrategy({
-	    clientId: mockClientId + '/',
-	  }, function() {});
-	}).to.throw(TypeError, 'IndieAuthStrategy requires a redirectUri option');
-      });
-      
-    }); // without a redirectUri option
-
-    describe('with an invalid responseType', function() {
-
-      it('should throw', function() {
-	expect(function() {
-	  var strategy = new IndieAuthStrategy({
-	    clientId: mockClientId + '/',
-	    redirectUri: mockRedirectUri,
-	    responseType: 'invalid',
-	  }, function() {});
-	}).to.throw(TypeError, 'response_type must be one of either "id" or "code"');
-      });
-      
-    }); // with an invalid responseType
-    
-  });
-  
 });
--- test/strategy.verify.test.js
+++ test/strategy.verify.test.js
@@ -20,100 +20,100 @@ var mockState = 'abc';
 var mockAuthCode = '1234';
 
 describe('@rcarls/passport-indieauth', function() {
-  
-  describe('receiving authorization result', function () {
     
-    var strategy = new IndieAuthStrategy({
-      clientId: mockClientId + '/',
-      redirectUri: mockRedirectUri,
-    }, function(domain, scope, mfData, done) {
-      var user = {
-	me: domain,
-	scope: scope,
-      };
-      
-      return done(null, user, mfData);
-    });
-    
-    var authRequest;
-
-    beforeEach('mock the user\'s home page', function(done) {
-      nock(mockUserId)
-	.get('/')
-	.replyWithFile(200, __dirname + '/mocks/user-homepage.html');
-      
-      done();
-    });
-
-    beforeEach('mock the verify call', function(done) {
-      nock(mockAuthEndpoint)
-	.post('', {
-	  code: mockAuthCode,
-	  client_id: mockClientId + '/',
-	  redirect_uri: mockRedirectUri,
-	})
-	.reply(200, qs.stringify({ me: mockUserId + '/', }));
-      
-      done();
-    });
-
-    describe('with minimum required parameters', function() {
-
-      it('should succeed', function(done) {
-	chai.passport.use(strategy)
-	  .success(function(user, info) {
-	    expect(user.me).to.equal(mockUserId + '/');
-	    
-	    done();
-	  })
-	  .req(function(req) {
-	    req.query = {
-	      code: mockAuthCode,
-	      me: mockUserId + '/',
+    describe('receiving authorization result', function () {
+	
+	var strategy = new IndieAuthStrategy({
+	    clientId: mockClientId + '/',
+	    redirectUri: mockRedirectUri,
+	}, function(domain, scope, mfData, done) {
+	    var user = {
+		me: domain,
+		scope: scope,
 	    };
-	    req.session = {};
-	  }).authenticate();
-      });
+	    
+	    return done(null, user, mfData);
+	});
+	
+	var authRequest;
 
-    }); // with minimum required parameters
-    
-    describe('without a code parameter', function() {
-      
-      it('should fail', function(done) {
-	chai.passport.use(strategy)
-	  .fail(function(info) {
-	    expect(info.message).to.equal('Missing request parameters');
+	beforeEach('mock the user\'s home page', function(done) {
+	    nock(mockUserId)
+		.get('/')
+		.replyWithFile(200, __dirname + '/mocks/user-homepage.html');
 	    
 	    done();
-	  })
-	  .req(function(req) {
-	    req.session = {};
-	  })
-	  .authenticate();
-	
-      });
+	});
 
-    }); // without a code parameter
-
-    describe('without a me parameter', function() {
-      
-      it('should fail', function(done) {
-	chai.passport.use(strategy)
-	  .fail(function(info) {
-	    expect(info.message).to.equal('Missing required "me" parameter');
+	beforeEach('mock the verify call', function(done) {
+	    nock(mockAuthEndpoint)
+		.post('', {
+		    code: mockAuthCode,
+		    client_id: mockClientId + '/',
+		    redirect_uri: mockRedirectUri,
+		})
+		.reply(200, qs.stringify({ me: mockUserId + '/', }));
 	    
 	    done();
-	  })
-	  .req(function(req) {
-	    req.query = { code: mockAuthCode, };
-	    req.session = {};
-	  })
-	  .authenticate();
-	
-      });
+	});
 
-    }); // without a code parameter
-      
-  }); // receiving authorization result
+	describe('with minimum required parameters', function() {
+
+	    it('should succeed', function(done) {
+		chai.passport.use(strategy)
+		    .success(function(user, info) {
+			expect(user.me).to.equal(mockUserId + '/');
+			
+			done();
+		    })
+		    .req(function(req) {
+			req.query = {
+			    code: mockAuthCode,
+			    me: mockUserId + '/',
+			};
+			req.session = {};
+		    }).authenticate();
+	    });
+
+	}); // with minimum required parameters
+	
+	describe('without a code parameter', function() {
+	    
+	    it('should fail', function(done) {
+		chai.passport.use(strategy)
+		    .fail(function(info) {
+			expect(info.message).to.equal('Missing request parameters');
+			
+			done();
+		    })
+		    .req(function(req) {
+			req.session = {};
+		    })
+		    .authenticate();
+		
+	    });
+
+	}); // without a code parameter
+
+	describe('without a me parameter', function() {
+	    
+	    it('should fail', function(done) {
+		chai.passport.use(strategy)
+		    .fail(function(info) {
+			expect(info.message).to.equal('Missing required "me" parameter');
+			
+			done();
+		    })
+		    .req(function(req) {
+			req.query = { code: mockAuthCode, };
+			req.session = {};
+		    })
+		    .authenticate();
+		
+	    });
+
+	}); // without a code parameter
+	
+    }); // receiving authorization result
 
 });
